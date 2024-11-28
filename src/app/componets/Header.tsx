8070b0b911f112a822f39e2f3082de238ee5e0e9
sumaira tahir <sumairatahir929@gmail.com>
2024-11-28 11:23:59 +0500

    trial 2

---
+++ src/app/componets/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+
+        <ul className="links">
+            <li><a rel="stylesheet" href="" /> workk</li>
+            <li><a rel="stylesheet" href="" /> SERVICES </li>
+            <li><a rel="stylesheet" href="" /> CONTACT</li>
+             
+             
+        </ul>
+      
+    </div>
+  );
+}
+
+export default Header;
